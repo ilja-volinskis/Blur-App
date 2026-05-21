3498666e3efea404373b6bb23f3e83150d921518
DIR\ilja.volinskis <email>
2026-05-21 09:40:52 +0300

    Create and use blur worker

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,9 +19,13 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
+import com.example.bluromatic.workers.BlurWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -29,11 +33,19 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
+    val workManager = WorkManager.getInstance(context)
+
+    private var imageUri: Uri = context.getImageUri()
+
     /**
      * Create the WorkRequests to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    override fun applyBlur(blurLevel: Int) {}
+    override fun applyBlur(blurLevel: Int) {
+        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+        workManager.enqueue(blurBuilder.build())
+    }
 
     /**
      * Cancel any ongoing WorkRequests
@@ -47,7 +59,8 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      */
     private fun createInputDataForWorkRequest(blurLevel: Int, imageUri: Uri): Data {
         val builder = Data.Builder()
-        builder.putString(KEY_IMAGE_URI, imageUri.toString()).putInt(KEY_BLUR_LEVEL, blurLevel)
+        builder.putString(KEY_IMAGE_URI, imageUri.toString())
+            .putInt(KEY_BLUR_LEVEL, blurLevel)
         return builder.build()
     }
 }
---
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -0,0 +1,54 @@
+package com.example.bluromatic.workers
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.delay
+
+private const val TAG = "BlurWorker"
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    override suspend fun doWork(): Result {
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.blurring_image),
+            applicationContext
+        )
+
+        return withContext(Dispatchers.IO) {
+            // This is an utility function added to emulate slower work.
+            delay(DELAY_TIME_MILLIS)
+
+            return@withContext try {
+                val bitmap = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+
+                val output = blurBitmap(bitmap, 1)
+                val outputUri = writeBitmapToFile(applicationContext, output)
+
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
+
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
+        }
+    }
+
+}
